7c4400fcc0f29ada227df88b1fcf78c0979d93f8
timlyeee <43355299+timlyeee@users.noreply.github.com>
2022-04-18 18:31:08 +0800

    daily code

--- courses/001_init_render_pipeline/core/application/Application.h
+++ courses/001_init_render_pipeline/core/application/Application.h
@@ -1,1 +1,17 @@
-#include "core/view/GLView.h"
+#include "core/renderWindow/GLRenderWindow.h"
+
+class Application {
+    
+    public:
+        Application();
+        ~Application();
+
+        //update scene data like vertex or mesh
+        updateScene();
+        //use file utils etc.
+        loadResources();
+        
+
+    private:
+        RenderWindow renderWindow;
+};
